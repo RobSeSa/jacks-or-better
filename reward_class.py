c27690178d9019b2b7c43e1a8a3dfb544f46d8c0
RobSeSa <robertssato@gmail.com>
2022-08-05 15:32:41 -0700

    Added checking for flush and straight flush

--- reward_class.py
+++ reward_class.py
@@ -18,24 +18,32 @@ Assume all inputs to indiv check functions are sorted integer version of hand
 '''
 
 import hand_class
+from hand_class import Hand
 import card_class
 from card_class import int_to_card, card_to_int
 
 REWARDS = {
+    'j_or_better':1,
     'pair':2,
-    'triple':3
+    'triple':3,
+    'straight':4,
+    'flush':6,
+    'full_house':9,
+    'quad':25,
+    'straight_flush':50,
+    'royal_flush':800
     }
+'''
+each function returns either name_reward, value_reward
+    ex) 'royal_flush', 800
+or '', 0 if no reward
+'''
 
 #print(int_to_card)
 #print(card_to_int)
 
-def is_straight(hand):
-    for i in range(len(hand)-1):
-        if not hand[i] + 1== hand[i+1]:
-            return False
-    return True
-
-def is_flush(hand):
+def check_flushes(hand):
+    # handles flush, straight flush and royal flush
     low_card = hand[0]
     high_card = hand[-1]
 
@@ -43,10 +51,35 @@ def is_flush(hand):
     hearts = low_card > 12 and high_card <= 25
     clubs = low_card > 25 and high_card <= 38
     diamonds = low_card > 38
+    is_flush = spades or hearts or clubs or diamonds
 
+    if is_flush:
+        straight_val = is_straight_flush(hand) # 0 = not straight, 1 = straight flush, 2 = royal straight flush
+        if straight_val == 0:
+            return 'flush', REWARDS['flush']
+        elif straight_val == 1:
+            return 'straight_flush', REWARDS['straight_flush']
+        elif straight_val == 2:
+            return 'royal_flush', REWARDS['royal_flush']
+        else:
+            print("\n\n Error checking is_flush in reward_class.compute_rewards\nReceived straight_val:", straight_val)
+    return '', 0
+
+def is_straight_flush(hand):
+    # 0 = not straight, 1 = straight flush, 2 = royal straight flush
+
+    if hand[-1] - hand[0] == 4: # if max card is 4+ min card (2,3,4,5,6)
+        return 1
+    # check if royal flush straight: 0,9,10,11,12
+    spades = hand == [0,9,10,11,12]
+    hearts = hand == [13,22,23,24,25]
+    clubs = hand == [26,35,36,37,38]
+    diamonds = hand == [39,48,49,50,51]
     if spades or hearts or clubs or diamonds:
-        return True
-    return False
+        return 2
+    return 0
+
+
 
 def compute_rewards(hand):
     # convert the hand from Card, Hand objects to raw ints
@@ -59,3 +92,21 @@ def compute_rewards(hand):
     print("After sorting:", hand_as_nums)
 
     # check each win type
+    print(is_straight_flush(hand_as_nums))
+
+
+
+if __name__=='__main__':
+    # Test straights
+    straight_2_6 = Hand()
+    for card_num in range(1,6):
+        straight_2_6.add_card(card_num)
+    print(straight_2_6)
+    compute_rewards(straight_2_6)
+
+
+    straight_A_5 = Hand()
+    for card_num in range(5):
+        straight_A_5.add_card(card_num)
+    print(straight_A_5)
+    compute_rewards(straight_A_5)
